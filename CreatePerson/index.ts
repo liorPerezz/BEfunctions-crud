20d47adea15ec3286a5dffe5ed71b66add9ecde8
leeor peretz <liorhorse@gmail.com>
2021-08-26 11:14:08 +0300

    feat: create person

--- CreatePerson/index.ts
+++ CreatePerson/index.ts
@@ -1,17 +1,18 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { PersonModel } from '../shared/personUser/person.model';
+import getConnection from '../shared/mogoose';
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    // context.res = {
-    //     // status: 200, /* Defaults to 200 */
-    //     body: responseMessage
-    // };
-
+const createPerson: AzureFunction = async (
+  context: Context,
+  req: HttpRequest
+): Promise<void> => {
+  try {
+    await getConnection();
+    const person = await PersonModel.create(req.body);
+    // findPerson: const person111 = await PersonModel.findById(person.id);
+  } catch (err) {
+    console.log(`couldnt create person ${err}`);
+  }
 };
 
-export default httpTrigger;
+export default createPerson;
---
+++ shared/personUser/person.model.ts
@@ -0,0 +1,13 @@
+import * as mongoose from 'mongoose';
+import { PersonDocument } from './person.interface';
+
+const personSchema = new mongoose.Schema({
+  name: mongoose.Schema.Types.String,
+  inGroups: {type: [mongoose.Schema.Types.ObjectId], ref: "Group" }
+});
+
+export const PersonModel: any = mongoose.model(
+  'Person',
+  personSchema,
+  'people'
+);

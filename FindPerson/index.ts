6747a4cfcb90bd2de705c2c9b565fc2b4e864bbf
leeor peretz <liorhorse@gmail.com>
2021-08-26 14:42:06 +0300

    feat: find person function

--- FindPerson/index.ts
+++ FindPerson/index.ts
@@ -2,17 +2,18 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { PersonModel } from '../shared/personUser/person.model';
 import getConnection from '../shared/mogoose';
 
-const FindPerson: AzureFunction = async function (
+const findPerson: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
   try {
     await getConnection();
     const person = await PersonModel.findById(context.bindingData.personId);
-    console.log('found person :', person);
+    // console.log('found person :', person);
+    return person;
   } catch (err) {
     console.log(`couldnt find person ${err}`);
   }
 };
 
-export default FindPerson;
+export default findPerson;

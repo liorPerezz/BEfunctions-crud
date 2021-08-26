c4e29b47063dc40bfe9b754269382390ee5ae53e
leeor peretz <liorhorse@gmail.com>
2021-08-26 14:42:48 +0300

    feat: update person function

---
+++ UpdatePerson/index.ts
@@ -0,0 +1,27 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { PersonModel } from '../shared/personUser/person.model';
+import getConnection from '../shared/mogoose';
+import FindPerson from '../FindPerson';
+
+const updatePerson: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+ 
+    try{
+       await getConnection();
+      const currentPersonId = context.bindingData.personId;
+       const currentPerson = await FindPerson(context,
+        req,currentPersonId);
+        // console.log('currrrr', currentPerson)
+       const updatedPerson = req.body
+    //    console.log('newwww' , updatedPerson);
+       
+       const ppp=  await PersonModel.updateOne({personName : currentPerson.name }, { newPersonName: updatedPerson.name}).exec() 
+       console.log( 'i hope this works ',ppp)
+     }
+   catch(err){
+       console.log('error');
+       
+   }
+    };
+
+
+export default updatePerson;

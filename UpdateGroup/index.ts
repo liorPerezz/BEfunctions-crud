b329b36850b7d1687477d994f12fe1a7cb6c9694
leeor peretz <liorhorse@gmail.com>
2021-08-29 16:13:09 +0300

    refactor: erased comments

--- UpdateGroup/index.ts
+++ UpdateGroup/index.ts
@@ -1,15 +1,13 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { GroupModel } from '../shared/groupUser/group.model';
 import getConnection from '../shared/mogoose';
-import FindGroup from '../FindGroup';
+
 
 const updateGroup: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
  
     try{
        await getConnection();
       const currentGroupId = context.bindingData.groupId;
-       const currentGroup = await FindGroup(context,
-        req,currentGroupId);
        const updatedGroup = req.body
        
        const ppp=  await GroupModel.findByIdAndUpdate(currentGroupId,updatedGroup,{new:true}).exec() 
--- UpdatePerson/index.ts
+++ UpdatePerson/index.ts
@@ -1,21 +1,15 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { PersonModel } from '../shared/personUser/person.model';
 import getConnection from '../shared/mogoose';
-import FindPerson from '../FindPerson';
 
 const updatePerson: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
  
     try{
        await getConnection();
       const currentPersonId = context.bindingData.personId;
-       const currentPerson = await FindPerson(context,
-        req,currentPersonId);
-        // console.log('currrrr', currentPerson)
        const updatedPerson = req.body
-    //    console.log('newwww' , updatedPerson);
-       
        const ppp=  await PersonModel.findByIdAndUpdate(currentPersonId,updatedPerson,{new:true}).exec() 
-       console.log( 'i hope this works ',ppp)
+       console.log('',ppp)
      }
    catch(err){
        console.log('error');
